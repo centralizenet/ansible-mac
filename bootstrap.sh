8f8004091bad11e2a78720590ec71f3cd019aa0c
Carl Allen <github@allenofmn.com>
2022-03-28 15:53:26 -0500

    add homebrew to zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@ xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
 
 brew install ansible
